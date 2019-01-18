cafa2d535a2c5b227ea62f955388eb8852f5a895
lalorosas <lalorosas@outlook.com>
2019-01-17 20:23:12 -0700

    Added App Center functionality

--- ExpensesExample/App.xaml.cs
+++ ExpensesExample/App.xaml.cs
@@ -4,6 +4,10 @@ using Xamarin.Forms.Xaml;
 using ExpensesExample.View;
 using Microsoft.WindowsAzure.MobileServices;
 using ExpensesExample.Resources;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Push;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace ExpensesExample
@@ -24,7 +28,7 @@ namespace ExpensesExample
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            AppCenter.Start("ios=344791c1-9a35-4ef2-acbd-1d160a1cbbae;android=718a8554-36ac-4f94-b80f-8701258d1f49", typeof(Analytics), typeof(Crashes), typeof(Push));
         }
 
         protected override void OnSleep()
